571aefa60284377035c71b4da042f2dc970f5edc
nil-tyou <nil_tyou@sina.com>
2013-05-16 20:55:14 +0800

    add ui test

---
+++ TestUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUI : MonoBehaviour {
+
+//	public Texture2D texBG;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI() {
+		GUI.Box(new Rect(100, 100, 100, 100), "UnityTest");
+	}
+}
